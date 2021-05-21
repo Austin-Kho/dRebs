99f794c842f7b4e1910fb27e97664c3fddd944d3
Austin-Kho <kori.susie@gmail.com>
2021-05-21 15:47:14 +0900

    board model build img ..

--- django/board/models.py
+++ django/board/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 
 class Group(models.Model):
-    key = models.CharField('키', max_length=100)
     name = models.CharField('명칭', max_length=255)
     order = models.PositiveSmallIntegerField('순서')
     manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name='관리자')
@@ -11,17 +10,14 @@ class Group(models.Model):
 
 class Board(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    key = models.CharField('키', max_length=100)
     name = models.CharField('명칭', max_length=255)
     order = models.PositiveSmallIntegerField('순서')
-    # search = models.PositiveSmallIntegerField('검색수')
     manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, verbose_name='관리자')
 
 
 class Category(models.Model):
     board = models.ForeignKey(Board, on_delete=models.SET_NULL)
-    key = models.CharField('키', max_length=255)
-    value = models.CharField('값', max_length=255)
+    name = models.CharField('값', max_length=255)
     parent = models.CharField('부모카테고리', max_length=255)
     order = models.PositiveSmallIntegerField('순서')
 
@@ -39,11 +35,31 @@ class Post(models.Model):
     ip = models.GenericIPAddressField()
     images = models.ImageField()
     files = models.FilePathField()
+    like = models.PositiveIntegerField()
+    dislike = models.PositiveIntegerField()
+    blame = models.PositiveSmallIntegerField()
     soft_delete = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
 
+class Tag(models.Model):
+    tag = models.CharField('태그', max_length=100)
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL)
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    reply = models.CharField('', max_length=20)
+    html_type = models.CharField('', choices=(('1', 'html'), ('2', 'text'), ('3', 'aa'), ('4', 'bb')))
+    secret = models.BooleanField(default=False)
+    content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    password = models.CharField('패스워드', max_length=255)
+    ip = models.GenericIPAddressField()
+    like = models.PositiveIntegerField()
+    dislike = models.PositiveIntegerField()
+    blame = models.PositiveSmallIntegerField()
+    soft_delete = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
