f509a7959f3ba3d00f9998c7d2d7cc48a9a5c976
Austin-Kho <kori.susie@gmail.com>
2021-05-23 23:30:37 +0900

    board model & admin update

--- django/board/admin.py
+++ django/board/admin.py
@@ -5,32 +5,63 @@ from . models import Group, Board, Partition, Category, Post, Comment, Tag
 
 
 class GroupAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ('id', 'name')
-    list_display_links = ('name',)
+    search_fields = ('name',)
+
+
+class PartitionInline(admin.TabularInline):
+    model = Partition
+    extra = 1
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+    extra = 1
 
 
 class BoardAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+    list_display = ('group', 'url', 'name', 'order', 'search_able')
+    list_display_links = ('name',)
+    list_editable = ('group', 'url', 'order', 'search_able')
+    search_fields = ('url', 'name')
+    list_filter = ('group',)
+    inlines = (PartitionInline, CategoryInline)
 
 
 class PartitionAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+    list_display = ('board', 'name', 'project', 'order')
+    list_display_links = ('name',)
+    list_editable = ('board', 'project', 'order')
+    search_fields = ('name',)
+    list_filter = ('board', 'project')
 
 
 class CategoryAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+    list_display = ('board', 'name', 'parent', 'order')
+    list_display_links = ('name',)
+    list_editable = ('board', 'parent', 'order')
+    search_fields = ('name',)
+    list_filter = ('board',)
 
 
 class PostAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+    list_display = ('board', 'is_notice', 'category', 'title', 'user')
+    list_display_links = ('title',)
+    list_editable = ('board', 'is_notice', 'category')
+    search_fields = ('title', 'content')
+    list_filter = ('board', 'is_notice', 'category')
 
 
 class CommentAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+    list_display = ('id', 'post', 'content', 'user')
+    list_display_links = ('content',)
+    search_fields = ('content', 'user')
 
 
 class TagAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+    list_display = ('board', 'tag')
+    list_editable = ('tag',)
+    search_fields = ('tag',)
+    list_filter = ('board',)
 
 
 admin.site.register(Group, GroupAdmin)
