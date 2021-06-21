16c1ef535c869d2e0276a7e2d964d79ec5a53f05
Austin-Kho <kori.susie@gmail.com>
2021-06-22 08:43:06 +0900

    link & file form update

--- django/rebs_docs/forms.py
+++ django/rebs_docs/forms.py
@@ -2,12 +2,12 @@ from django import forms
 
 from board.models import Post, Link, File
 
-
 LinkInlineFormSet = forms.models.inlineformset_factory(
     Post,
     Link,
     fields=['link'],
     extra=1,
+    can_delete=True,
     can_delete_extra=False
 )
 
@@ -16,5 +16,6 @@ FileInlineFormSet = forms.models.inlineformset_factory(
     File,
     fields=['file'],
     extra=1,
+    can_delete=True,
     can_delete_extra=False
 )
