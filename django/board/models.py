0186de5d6c4ae26293d35e6875fb71241846e03a
Austin-Kho <kori.susie@gmail.com>
2021-06-24 01:31:24 +0900

    lawsuit model update

--- django/board/models.py
+++ django/board/models.py
@@ -53,7 +53,7 @@ class LawsuitCase(models.Model):
     project = models.ForeignKey('rebs_project.Project', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='프로젝트')
     sort = models.CharField('구분', max_length=1, choices=(('1', '민사'), ('2', '형사'), ('3', '행정'), ('4', '가사'), ('5', '신청/집행')))
     level = models.CharField('심급', max_length=1, choices=(('0', '신청/집행'), ('1', '1심'), ('2', '2심'), ('3', '3심')), null=True, blank=True)
-    related_case = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='관련사건', help_text='본안사건인 경우 이전 심급 사건, 신청사건인 경우 관련 본안 사건 지정')
+    related_case = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='관련사건', help_text='본안사건인 경우 이전 심급 사건, 신청/집행 사건인 경우 관련 본안 사건 지정')
     case_number = models.CharField('사건번호(사건명)', max_length=50)
     plaintiff = models.CharField('원고(신청인)', max_length=20)
     defendant = models.CharField('피고(피신청인)', max_length=20)
