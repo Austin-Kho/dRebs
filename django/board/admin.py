fba06ccc40f4c86f65cac997e18f048d7a458f90
Austin-Kho <kori.susie@gmail.com>
2021-06-24 15:32:16 +0900

    board admin update

--- django/board/admin.py
+++ django/board/admin.py
@@ -32,9 +32,9 @@ class CategoryAdmin(ImportExportMixin, admin.ModelAdmin):
 
 
 class LawsuitCaseAdmin(ImportExportMixin, admin.ModelAdmin):
-    list_display = ('project', 'sort', 'level', 'related_case', 'case_number', 'plaintiff', 'defendant', 'case_start_date')
-    list_display_links = ('case_number',)
-    list_editable = ('project', 'sort', 'level', 'related_case', 'case_start_date',)
+    list_display = ('project', 'sort', 'level', '__str__', 'plaintiff', 'defendant', 'case_start_date')
+    list_display_links = ('__str__',)
+    list_editable = ('project', 'sort', 'level', 'case_start_date',)
     list_filter = ('project', 'sort', 'level')
     search_fields = ('case_number', 'plaintiff', 'defendant')
 
